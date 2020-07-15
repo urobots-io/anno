7784f0f464b1370aa891a26873c24c6813b2c206
Alexey Pavlov <pavlov_7@yahoo.de>
2020-07-15 12:19:00 +0200

    version.cpp temporarily added to the repository, call SubWCRev removed from the prebuild event

---
+++ anno/version.cpp
@@ -0,0 +1,15 @@
+// application version template
+
+namespace version {
+
+const char *revision      = "-";
+const char *modified      = "-";
+const char *date          = "2020/06/29 13:28:03";
+const char *date_UTC       = "2020/06/29 11:28:03";
+const char *time_now       = __DATE__ " " __TIME__;
+const char *rev_range      = "24456:24457";
+const char *mixed         = "-";
+const char *URL          = "-";
+const char *is_in_SVN       = "not versioned";
+
+}

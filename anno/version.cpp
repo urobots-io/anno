7df1ca2c28cdf52029509e336ff01c5c2217bda2
Alexey Pavlov <pavlov_7@yahoo.de>
2020-07-16 17:39:14 +0200

    cleanup version info

--- anno/version.cpp
+++ anno/version.cpp
@@ -4,10 +4,10 @@ namespace version {
 
 const char *revision      = "-";
 const char *modified      = "-";
-const char *date          = "2020/06/29 13:28:03";
-const char *date_UTC       = "2020/06/29 11:28:03";
+const char *date          = "";
+const char *date_UTC       = "";
 const char *time_now       = __DATE__ " " __TIME__;
-const char *rev_range      = "24456:24457";
+const char *rev_range      = "";
 const char *mixed         = "-";
 const char *URL          = "-";
 const char *is_in_SVN       = "not versioned";

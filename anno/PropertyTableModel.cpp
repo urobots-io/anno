05c5eb1484e4b1902cb8dc6c969cdde4db262737
Alexey Pavlov <pavlov_7@yahoo.de>
2021-10-03 19:41:18 +0200

    cpp headers updated

--- anno/PropertyTableModel.cpp
+++ anno/PropertyTableModel.cpp
@@ -1,7 +1,12 @@
+// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++, C#, and Java: http://www.viva64.com
+//
+// Anno Labeling Tool
+// 2020-2021 (c) urobots GmbH, https://urobots.io
+
 #include "PropertyTableModel.h"
 
 using namespace std;
-
 
 PropertyTableModel::PropertyTableModel(QObject *parent)
     : QAbstractTableModel(parent)
